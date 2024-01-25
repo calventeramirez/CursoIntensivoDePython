1d1821dacd6a199c13e4af61889df527eb9554b4
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2024-01-25 17:25:46 +0100

    Añadido el ultimo ejercicio del capitulo 4

---
+++ Capitulo4/4_13-Bufe.py
@@ -0,0 +1,12 @@
+tupla = ("Pizzas", "Patatas fritas", "Hamburguesas", "Arroz 3 delicias", "Nuggets")
+print("La carta del restaurante es: ")
+for comida in tupla:
+    print(comida)
+#Pruebo a intentar cambiar
+#tupla[0] = "Pizzas con piña"
+
+#Modificamos 2 elementos de la tupla
+tupla = ("Pizzas", "Patatas fritas", "Muerte por chocolate", "Helado", "Nuggets")
+print("La nueva carta del restaurante es: ")
+for comida in tupla:
+    print(comida)
