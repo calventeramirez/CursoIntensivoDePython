93e937fb796f538d74b5a5d19d2fac95bba51c86
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2024-12-27 20:44:25 +0100

    Ejercicio 1 del capitulo 7 realizado

---
+++ Capitulo7/7_1-CocheAlquiler.py
@@ -0,0 +1,5 @@
+# Escribir un programa que el usuario meta el nombre de un coche y compruebe si esta el coche
+
+marca = input('Introduzca el coche que quiere alquilar: ')
+
+print(f'Comprobando si esta el {marca} disponible.')
