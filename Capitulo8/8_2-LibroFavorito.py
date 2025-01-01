e4dab0ed7b714bf70d300c877ee1ea7a232a6716
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2025-01-01 03:22:36 +0100

    Ejercicio 2 del capitulo 8 realizado

---
+++ Capitulo8/8_2-LibroFavorito.py
@@ -0,0 +1,6 @@
+# Escribir la función libro_favorito() que acepte el parametro libro e imprimir el mensaje 'Mi libro favorito es...'.
+
+def libro_favorito(titulo):
+    print(f'Mi libro favorito es {titulo.title()}')
+
+libro_favorito('El nombre del viento')
