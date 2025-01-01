ac1781b3c802bb7fc1c0e22f6700ddf4be4237d0
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2025-01-01 04:26:22 +0100

    Ejercicio 5 del capitulo 8 realizado

---
+++ Capitulo8/8_5-Ciudades.py
@@ -0,0 +1,9 @@
+# Escribir funcion escribir_ciudad() que acepte una ciudad y un pais. Imprimir "ciudad esta en pais".
+# Parametros valores por defecto por mi cuenta
+
+def escribir_ciudad(ciudad = "Málaga", pais = "España"):
+    print(f'{ciudad} esta en {pais}')
+
+escribir_ciudad("Roma", "Italia")
+escribir_ciudad()
+escribir_ciudad("Paris", "Francia")
