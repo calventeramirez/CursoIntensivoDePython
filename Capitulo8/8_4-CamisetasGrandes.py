6938d47a5ca7b02d6a65361996f7f9ab4ac24fb0
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2025-01-01 04:21:36 +0100

    Ejercicio 4 del capitulo 8 realizado

---
+++ Capitulo8/8_4-CamisetasGrandes.py
@@ -0,0 +1,7 @@
+# Modificar la funcion anterior de hacer_camiseta() para que tenga por tdefecto la talla grande y el mensaje "Me encanta Python"
+
+def hacer_camiseta(talla='grande', mensaje='Me encanta Python'):
+    print(f'La talla seleccionada es { talla } y el mensaje a imprimir en la camiseta es "{ mensaje }"')
+
+hacer_camiseta('mediana', 'Hazlo o no lo hagas, no lo intentes')
+hacer_camiseta()
