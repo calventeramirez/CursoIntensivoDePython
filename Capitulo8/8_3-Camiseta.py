2efcfa981ddea57229d3769d0a02fd0e092b4130
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2025-01-01 04:16:28 +0100

    Ejercicio 3 del capitulo 8 realizado

---
+++ Capitulo8/8_3-Camiseta.py
@@ -0,0 +1,7 @@
+# Crear la función hacer_camiseta() que acepte la talla y un mensaje para imprimer en la camiseta.
+# Devolver la talla y el mensaje
+
+def hacer_camiseta(talla, mensaje):
+    print(f'La talla seleccionada es { talla } y el mensaje a imprimir en la camiseta es "{ mensaje }"')
+
+hacer_camiseta(38, 'Hazlo o no lo hagas pero no lo intentes')
