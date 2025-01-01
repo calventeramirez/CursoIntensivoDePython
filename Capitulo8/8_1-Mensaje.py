88ce46466e4f4a8f46a0721c5daf83a1ef371a7d
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2025-01-01 03:18:49 +0100

    Ejercicio 1 del capitulo 8 realizado

---
+++ Capitulo8/8_1-Mensaje.py
@@ -0,0 +1,6 @@
+# Escibir una función llamada mostrar_mensaje() que imprima algun mensaje. Y ver que se ejecute ese mensaje.
+
+def mostrar_mensaje():
+    print('Estoy usando una función para sacar esta información!!!!')
+
+mostrar_mensaje()
