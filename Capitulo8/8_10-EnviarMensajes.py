65dcf39aba49b2b90cfab1dc2f26509d6a8307c9
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2025-01-02 21:21:46 +0100

    Ejercicio 10 del capitulo 8 realizado

---
+++ Capitulo8/8_10-EnviarMensajes.py
@@ -0,0 +1,17 @@
+def mostrar_mensajes(mensajes):
+    for mensaje in mensajes:
+        print(mensaje)
+
+def enviar_mensaje(mensajes, enviados):
+    while mensajes:
+        mensaje = mensajes.pop()
+        enviados.append(mensaje)
+
+mensajes = ['Hola Mundo', 'Hello Wordl', 'Adios', 'Good Bye']
+enviados = []
+
+mostrar_mensajes(mensajes)
+enviar_mensaje(mensajes[:], enviados)
+
+print(mensajes)
+print(enviados)
