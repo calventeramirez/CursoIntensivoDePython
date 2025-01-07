0c4558a73ab77e423df16aaf0569dfab5c22de3c
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2025-01-07 03:36:47 +0100

    Ejercicio 8 del capitulo 9 realizado

---
+++ Capitulo9/9_8-Privilegios.py
@@ -0,0 +1,45 @@
+class Usuario:
+    """Clase que representa a un usuario"""
+    def __init__(self, nombre, apellidos, nick, correo, clave):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.nick = nick
+        self.correo = correo
+        self.clave = clave
+        self.intentos_inicios = 0
+
+    def describir_usuario(self):
+        """Saca por pantalla la informacion del usuario"""
+        print(f'Nombre: {self.nombre}\nApellidos: {self.apellidos}\nNick: {self.nick}\nClave: {self.clave}\nCorreo: {self.correo}\n')
+
+    def saludar_usuario(self):
+        """Imprimir saludo personalizado"""
+        print(f'Bienvenido {self.nick}')
+
+    def incrementar_intentos_inicios(self):
+        """Incrementa a en 1 el numero de intentos de iniciar sesion"""
+        self.intentos_inicios += 1
+
+    def restablecer_intentos_incios(self):
+        """Restablece a 0 el numero de intentos de iniciar sesion"""
+        self.intentos_inicios = 0
+
+class Privilegios:
+    def __init__(self):
+        self.privilegios = ['Modificar permisos', 'Añadir Usuarios', 'Añadir comentarios', 'Excluir usuarios', 'Crear listas']
+
+    def show_privileges(self):
+        """Muestra la listas de privilegios de los usuarios"""
+        i = 1
+        print('Listas de privilegios: ')
+        for privilegio in self.privilegios:
+            print(f'{i} - {privilegio}')
+            i += 1
+
+class Admin(Usuario):
+    def __init__(self, nombre, apellidos, nick, correo, clave):
+        super().__init__(nombre,apellidos,nick, correo, clave)
+        self.privilegios = Privilegios()
+
+administrador = Admin('Pepe', 'Burgos', 'pburgos', 'pburgos@gmail.com', '123456')
+administrador.privilegios.show_privileges()
